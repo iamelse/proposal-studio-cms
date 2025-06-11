b236959e271461bc0f1a98103c4cabc1b2e03d4d
Lana Septiana <lana.septiana2@gmail.com>
2025-06-11 13:34:10 +0700

    fix image helper base path

--- app/Helpers/ImageHelper.php
+++ app/Helpers/ImageHelper.php
@@ -188,7 +188,7 @@ if (!function_exists('getPostCoverImagePath')) {
     {
         $disk = env('FILESYSTEM_DISK');
         $appUrl = rtrim(env('APP_URL'), '/');
-        $publicHtmlPath = base_path('../../proposal-studio.iamelse.my.id');
+        $publicHtmlPath = base_path('/../../proposal-studio.iamelse.my.id');
         $placeholderUrl = 'https://picsum.photos/1200/600?random=' . ($post->id ?? 'null');
 
         Log::info('getPostCoverImagePath called', [
